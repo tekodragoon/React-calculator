a29e437093d1ee2b28dda20779cf452d48126ca7
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2022-12-14 12:26:53 +0100

    Add mult and div function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,9 +8,11 @@ export class Calculator extends Component {
     this.state = {
       operandeA: "",
       operandeB: "",
-      operation: "",
+      operandeC: "",
+      operationAB: "",
+      operationAC: "",
       inProgress: false,
-    }
+    };
   }
 
   renderNumber(i) {
@@ -52,71 +54,138 @@ export class Calculator extends Component {
         }
         return;
       case "egal":
-        if (this.state.operandeB.length === 0) return;
-        let result = this.getOperationResult();
+        if (
+          this.state.operandeB.length === 0 &&
+          this.state.operandeC.length === 0
+        )
+          return;
+        let result = this.state.operationAC !== ""
+          ? this.getFullResult()
+          : this.getOperationABResult();
+
         this.setState({
           operandeA: result,
           operandeB: "",
+          operandeC: "",
           inProgress: true,
-          operation: "",
-        })
+          operationAB: "egal",
+          operationAC: "",
+        });
         return;
       case "plus":
-        if (this.state.inProgress) {
+        if (this.state.inProgress && this.state.operationAB !== "egal") {
           this.setState({
             inProgress: true,
-            operation: "plus",
-          })
+            operationAB: "plus",
+          });
           return;
         }
-        if (this.state.operandeB.length > 0) {
-          let result = this.getOperationResult();
+        if (this.state.operandeB.length > 0 || this.state.operationAC !== "") {
+          let result = this.state.operationAC !== ""
+          ? this.getFullResult()
+          : this.getOperationABResult();
           this.setState({
             operandeA: result,
             operandeB: result,
+            operandeC: "",
             inProgress: true,
-            operation: "plus",
-          })
+            operationAB: "plus",
+            operationAC: "",
+          });
           return;
         }
         this.setState({
           operandeB: this.state.operandeA,
           inProgress: true,
-          operation: "plus",
+          operationAB: "plus",
         });
         return;
-        case "min":
-          if (this.state.inProgress) {
-            this.setState({
-              inProgress: true,
-              operation: "min",
-            })
-            return;
-          }
-          if (this.state.operandeB.length > 0) {
-            let result = this.getOperationResult();
-            this.setState({
-              operandeA: result,
-              operandeB: result,
-              inProgress: true,
-              operation: "min",
-            })
-            return;
-          }
+      case "min":
+        if (this.state.inProgress && this.state.operationAB !== "egal") {
           this.setState({
-            operandeB: this.state.operandeA,
             inProgress: true,
-            operation: "min",
+            operationAB: "min",
           });
           return;
+        }
+        if (this.state.operandeB.length > 0 || this.state.operationAC !== "") {
+          let result = this.state.operationAC !== ""
+          ? this.getFullResult()
+          : this.getOperationABResult();
+          this.setState({
+            operandeA: result,
+            operandeB: result,
+            operandeC: "",
+            inProgress: true,
+            operationAB: "min",
+            operationAC: "",
+          });
+          return;
+        }
+        this.setState({
+          operandeB: this.state.operandeA,
+          inProgress: true,
+          operationAB: "min",
+        });
+        return;
+      case "mult":
+        if (this.state.inProgress && this.state.operationAB !== "egal") {
+          this.setState({
+            inProgress: true,
+            operationAB: "",
+            operationAC: "mult",
+          });
+          return;
+        }
+        if (this.state.operandeC.length > 0) {
+          let result = this.getOperationACResult();
+          this.setState({
+            operandeA: result,
+            operandeC: result,
+            inProgress: true,
+            operationAC: "mult",
+          });
+          return;
+        }
+        this.setState({
+          operandeC: this.state.operandeA,
+          inProgress: true,
+          operationAC: "mult",
+        });
+        return;
+      case "div":
+        if (this.state.inProgress && this.state.operationAB !== "egal") {
+          this.setState({
+            inProgress: true,
+            operationAB: "",
+            operationAC: "div",
+          });
+          return;
+        }
+        if (this.state.operandeC.length > 0) {
+          let result = this.getOperationACResult();
+          this.setState({
+            operandeA: result,
+            operandeC: result,
+            inProgress: true,
+            operationAC: "div",
+          });
+          return;
+        }
+        this.setState({
+          operandeC: this.state.operandeA,
+          inProgress: true,
+          operationAC: "div",
+        });
+        return;
       default:
         return;
     }
   }
 
-  getOperationResult() {
+  getOperationABResult() {
     let result;
-    switch (this.state.operation) {
+    switch (this.state.operationAB) {
       case "plus":
         result = Number(this.state.operandeB) + Number(this.state.operandeA);
         return this.reduceFloat(result);
@@ -128,12 +197,44 @@ export class Calculator extends Component {
     }
   }
 
+  getOperationACResult() {
+    let result;
+    switch (this.state.operationAC) {
+      case "mult":
+        result = Number(this.state.operandeC) * Number(this.state.operandeA);
+        return result;
+      case "div":
+        result = Number(this.state.operandeC) / Number(this.state.operandeA);
+        return result;
+      default:
+        return "";
+    }
+  }
+
+  getFullResult() {
+    let fullResult;
+    let resultAC = Number(this.getOperationACResult());
+    switch (this.state.operationAB) {
+      case "plus":
+        fullResult = resultAC + Number(this.state.operandeB);
+        break;
+      case "min":
+        fullResult = Number(this.state.operandeB) - resultAC;
+        break;
+      default:
+        fullResult = resultAC;
+        break;
+    }
+    return fullResult.toString();
+  }
+
   reduceFloat(f) {
     let a = this.state.operandeA.split(".");
     let b = this.state.operandeB.split(".");
-    let maxDecimal = (a.length > 1 ? a[1].length : 0) + (b.length > 1 ? b[1].length : 0);
+    let maxDecimal =
+      (a.length > 1 ? a[1].length : 0) + (b.length > 1 ? b[1].length : 0);
     let res = f.toFixed(maxDecimal).toString();
-    while(res[res.length - 1] === "0") {
+    while (res[res.length - 1] === "0") {
       res = res.slice(0, -1);
     }
     return res;
@@ -153,6 +254,8 @@ export class Calculator extends Component {
         return "-";
       case "plus":
         return "+";
+      case "reset":
+        return "AC";
       default:
         return "";
     }
@@ -161,9 +264,8 @@ export class Calculator extends Component {
   render() {
     return (
       <div className="calc">
-        <CalcScreen 
-          text={this.state.operandeA}
-        />
+        <CalcScreen text={this.state.operandeA} />
+        <div>{this.renderSpecialKey("reset")}</div>
         <div>
           {this.renderNumber(7)}
           {this.renderNumber(8)}
